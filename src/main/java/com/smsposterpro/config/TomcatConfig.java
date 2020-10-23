1135d8f0ef1527818883c34fea0ff0afb4f06000
刘东阳 <liudongyangds@chinamobile.com>
2020-10-23 09:49:44 +0800

    ssl

---
+++ src/main/java/com/smsposterpro/config/TomcatConfig.java
@@ -0,0 +1,62 @@
+package com.smsposterpro.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class TomcatConfig {
+
+    private int httpPort = 8080;
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.setPort(httpPort);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+
+            @Override
+            protected void postProcessContext(Context context) {
+
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    /**
+     * 让我们的应用支持HTTP是个好想法，但是需要重定向到HTTPS，
+     * 但是不能同时在application.properties中同时配置两个connector，
+     * 所以要以编程的方式配置HTTP connector，然后重定向到HTTPS connector
+     *
+     * @return Connector
+     */
+    private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        // http端口
+        connector.setPort(9090);
+        connector.setSecure(false);
+        // application.properties中配置的https端口
+        connector.setRedirectPort(8080);
+        return connector;
+    }
+
+}
